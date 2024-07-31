0cab7a473f1c0aa3f3c23f987978dcda7318b851
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-07-31 11:50:15 -0600

    fixed physics pixels not being drawn at the right position

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 local image = nil
 local pixels = {}
-local sizeX = 20
-local sizeY = 20
+local sizeX = 15
+local sizeY = 15
 local spacing = 1
 
 local physicsInstace = require("yan.instance.physics_instance")
@@ -9,12 +9,12 @@ local physicsInstace = require("yan.instance.physics_instance")
 function love.load()
     world = love.physics.newWorld(0,300,true)
 
-    wall = physicsInstace:New(nil, world, "static", "rectangle", {X = 500, Y = 50}, 0, 0)
-    wall.body:setX(300)
+    wall = physicsInstace:New(nil, world, "static", "rectangle", {X = 2000, Y = 50}, 0, 0, {X = 0, Y = 500})
+    wall.body:setX(0)
     wall.body:setY(500)
     wall:SetColor(1,1,1,1)
     wall.Shape = "rectangle"
-    wall.Size = {X = 500, Y = 50}
+    wall.Size = {X = 2000, Y = 50}
 end
 
 function love.update(dt)
@@ -36,7 +36,7 @@ function love.update(dt)
             x:ApplyForce(-500, 0)
         end
     end
-
+    
     if love.keyboard.isDown("d") then
         for _, x in ipairs(pixels) do
             x:ApplyForce(500, 0)
@@ -48,26 +48,24 @@ function love.draw()
     for _, x in ipairs(pixels) do
         x:Draw()
     end
-
+    
     wall:Draw()
 end
 
 function love.filedropped(file)
     pixels = {}
-
+    
     file:open("r")
     fileData = file:read("data")
     image = love.image.newImageData(fileData)
     
     local i = 1
-
+    
     for x = 0, image:getWidth() - 1 do
         for y = 0, image:getHeight() - 1 do
             local r, g, b, a = image:getPixel(x,y)
-
-            pixels[i] = physicsInstace:New(nil, world, "dynamic", "rectangle", {X = sizeX, Y = sizeY}, 0, 0)
-            pixels[i].body:setX(x * sizeX * spacing)
-            pixels[i].body:setY(y * sizeY * spacing)
+            
+            pixels[i] = physicsInstace:New(nil, world, "dynamic", "rectangle", {X = sizeX, Y = sizeY}, 0, 0, {X = x * sizeX * spacing, Y = y * sizeY * spacing})
             pixels[i]:SetColor(r,g,b,a)
             pixels[i].Shape = "rectangle"
             pixels[i].Size = {X = sizeX, Y = sizeY}
--- yan/instance/physics_instance.lua
+++ yan/instance/physics_instance.lua
@@ -5,11 +5,11 @@ local physicsInstance = {}
 local instance = require("yan.instance.instance")
 physicsInstance.__index = instance
 
-function physicsInstance:New(o, world, bodyType, shape, size, restitution, damping)
+function physicsInstance:New(o, world, bodyType, shape, size, restitution, damping, pos)
     o = o or instance:New(o)
     setmetatable(o, self)
     o.Type = "PhysicsInstance"
-    o.body = love.physics.newBody(world, o.Position.X, o.Position.Y, bodyType)
+    o.body = love.physics.newBody(world, pos.X, pos.Y, bodyType)
     
     if shape == "rectangle" then
         o.shape = love.physics.newRectangleShape(size.X, size.Y)
@@ -21,23 +21,23 @@ function physicsInstance:New(o, world, bodyType, shape, size, restitution, dampi
     o.body:setLinearDamping(damping)
     
     function o:Update()
-        if self.SceneEnabled == false then return end
+        if o.SceneEnabled == false then return end
 
-        self.Position.X = self.body:getX()
-        self.Position.Y = self.body:getY()
+        o.Position.X = o.body:getX()
+        o.Position.Y = o.body:getY()
     end 
 
     function o:ApplyForce(x, y)
-        if self.SceneEnabled == false then return end
-        self.body:applyForce(x, y)
+        if o.SceneEnabled == false then return end
+        o.body:applyForce(x, y)
     end
 
     function o:ApplyLinearImpulse(x, y, maxX, maxY)
-        if self.SceneEnabled == false then return end
-        self.body:applyLinearImpulse(x, y)
+        if o.SceneEnabled == false then return end
+        o.body:applyLinearImpulse(x, y)
         
         if maxX and maxY then
-            local vX, vY = self.body:getLinearVelocity()
+            local vX, vY = o.body:getLinearVelocity()
 
             if vX > maxX then 
                 vX = maxX
@@ -45,7 +45,29 @@ function physicsInstance:New(o, world, bodyType, shape, size, restitution, dampi
                 vX = -maxX 
             end
 
-            self.body:setLinearVelocity(vX, vY)
+            o.body:setLinearVelocity(vX, vY)
+        end
+    end
+    
+    function o:Draw()
+        if o.SceneEnabled == false then return end
+        love.graphics.setColor(
+            o.Color.R,
+            o.Color.G,
+            o.Color.B,
+            o.Color.A
+        )
+        
+        love.graphics.polygon("fill", o.body:getWorldPoints(o.shape:getPoints()))
+        
+        if o.Shape == "rectangle" then
+            --[[love.graphics.rectangle(
+                "fill",
+                o.body:getX(),
+                o.body:getY(),
+                size.X,
+                size.Y
+            )]]
         end
     end
     
